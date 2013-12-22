c6b7216ab0ac7e3417d4a3c789d5c94e10a62ded
Ayax50000 <fjlsoto@gmail.com>
2013-12-22 14:52:25 -0600

    level 5 working

--- player.rb
+++ player.rb
@@ -6,10 +6,14 @@ class Player
     # add your code here
     puts warrior.health
     warrior.feel
+    if warrior.feel.captive? then
+        warrior.rescue!
+    else
     warrior.attack! unless warrior.feel.empty?
     if warrior.feel.empty? then
         warrior.rest! if warrior.health < 20 && @health == warrior.health
         warrior.walk! if warrior.health == 20 || @health > warrior.health
+    end
     end
     @health = warrior.health
   end
