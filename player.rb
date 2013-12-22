990ed09690bb03bd978576e68c4ff1541746e51b
Ayax50000 <fjlsoto@gmail.com>
2013-12-22 14:39:59 -0600

    level 4 working

--- player.rb
+++ player.rb
@@ -1,12 +1,16 @@
 class Player
+  def initialize
+      @health = 20
+  end
   def play_turn(warrior)
     # add your code here
     puts warrior.health
     warrior.feel
     warrior.attack! unless warrior.feel.empty?
     if warrior.feel.empty? then
-        warrior.rest! unless warrior.health == 20 
-        warrior.walk! if warrior.health == 20
+        warrior.rest! if warrior.health < 20 && @health == warrior.health
+        warrior.walk! if warrior.health == 20 || @health > warrior.health
     end
+    @health = warrior.health
   end
 end
