65eea16da328eadf5cc22b4df79b281426a82af2
Ayax50000 <fjlsoto@gmail.com>
2013-12-22 14:20:09 -0600

    level 3 working

--- player.rb
+++ player.rb
@@ -1,8 +1,12 @@
 class Player
   def play_turn(warrior)
     # add your code here
+    puts warrior.health
     warrior.feel
     warrior.attack! unless warrior.feel.empty?
-    warrior.walk! if warrior.feel.empty?
+    if warrior.feel.empty? then
+        warrior.rest! unless warrior.health == 20 
+        warrior.walk! if warrior.health == 20
+    end
   end
 end
